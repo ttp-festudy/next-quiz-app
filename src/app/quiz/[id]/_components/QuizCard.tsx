c3d5e69574d009c27be4577563c847c60f23ed15
rlawlsdn263 <rlawlsdn263@naver.com>
2024-04-02 23:30:04 +0900

    [🗼마크업] `QuizCard` 컴포넌트 생성

---
+++ src/app/quiz/[id]/_components/QuizCard.tsx
@@ -0,0 +1,7 @@
+export default function QuizCard() {
+  return (
+    <div className="w-[300px] h-[400px] bg-[#6d6d6d] p-[20px] rounded-[20px] flex justify-center items-center">
+      <p className="font-[600] text-[20px]">문제 제목</p>
+    </div>
+  );
+}
