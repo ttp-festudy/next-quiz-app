0c27b0f75943f24d997d4843a79c5632117c3b4f
rlawlsdn263 <rlawlsdn263@naver.com>
2024-04-03 00:14:53 +0900

    [✨기능] `HomePage`에 퀴즈 시작 기능 추가
    시작하기를 누르면 `/quiz/0`으로 이동해 퀴즈를 시작하도록 했다.

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,9 +1,14 @@
-import QuizModal from "@/components/QuizModal";
+import Link from "next/link";
 
-export default function Home() {
+export default function HomePage() {
   return (
     <main className="flex justify-center items-center h-screen">
-      <QuizModal />
+      <Link
+        href={`/quiz/0`}
+        className="border px-[16px] py-[4px] rounded-[8px] hover:bg-[#454545]"
+      >
+        시작하기
+      </Link>
     </main>
   );
 }
