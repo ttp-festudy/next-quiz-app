a52a222e99ebc508476ffb997fd57a7cb476fbc5
rlawlsdn263 <rlawlsdn263@naver.com>
2024-04-02 23:22:00 +0900

    [✨기능] `QuizModal` 컴포넌트에 데이터 바인딩

--- src/components/QuizModal.tsx
+++ src/components/QuizModal.tsx
@@ -1,15 +1,13 @@
+import { cs } from "@/data/_index";
+
 export default function QuizModal() {
   return (
     <div className="w-[800px] h-[400px] bg-[#6d6d6d] p-[20px] rounded-[20px] flex flex-col gap-[20px]">
       <div className="bg-[#4f4f4f] p-[20px] rounded-[10px]">
-        <span className="font-[800]">MPA란 무엇인가?</span>
+        <span className="font-[800]">{cs[0].title}</span>
       </div>
       <div className="bg-[#4f4f4f] p-[20px] rounded-[10px] flex-1">
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis
-          voluptatem sint, officia inventore id ab eius sed necessitatibus hic.
-          Hic nesciunt veniam tempora ab. Impedit nemo facilis quos alias magni.
-        </p>
+        <p>{cs[0].content}</p>
       </div>
     </div>
   );
