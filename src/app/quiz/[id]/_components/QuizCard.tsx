5f76e1c41c75de47f3c1a275cc93758b786268df
rlawlsdn263 <rlawlsdn263@naver.com>
2024-04-03 00:36:48 +0900

    [✨기능] `SolutionPage`로 이동 기능 구현
    `Link`를 사용해 문제의 정답을 지닌 `SolutionPage`로 이동하는 기능을 추가했다.

--- src/app/quiz/[id]/_components/QuizCard.tsx
+++ src/app/quiz/[id]/_components/QuizCard.tsx
@@ -1,7 +1,16 @@
-export default function QuizCard() {
+import Link from "next/link";
+import { cs } from "@/data/_index";
+
+export default function QuizCard({ id }: { id: number }) {
   return (
-    <div className="w-[300px] h-[400px] bg-[#6d6d6d] p-[20px] rounded-[20px] flex justify-center items-center">
-      <p className="font-[600] text-[20px]">문제 제목</p>
+    <div className="w-[300px] h-[400px] bg-[#6d6d6d] p-[20px] rounded-[20px] flex justify-center items-center relative">
+      <p className="font-[600] text-[20px]">{cs[id].title}</p>
+      <Link
+        href={`/solution/${id}`}
+        className="absolute bottom-[20px] left-[20px] border px-[16px] py-[4px] rounded-[8px] hover:bg-[#454545]"
+      >
+        정답
+      </Link>
     </div>
   );
 }
