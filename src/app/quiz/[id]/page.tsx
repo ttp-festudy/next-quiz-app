b40acdbcc76a9183f2ad3eb9deffa3691224a976
rlawlsdn263 <rlawlsdn263@naver.com>
2024-04-02 23:42:55 +0900

    [✨기능] `params` 객체로 슬러그 접근

--- src/app/quiz/[id]/page.tsx
+++ src/app/quiz/[id]/page.tsx
@@ -1,6 +1,10 @@
 import { QuizCard } from "./_components/_index";
 
-export default function QuizPage() {
+export default function QuizPage({
+  params: { id },
+}: {
+  params: { id: string };
+}) {
   return (
     <main className="flex justify-center items-center h-screen">
       <QuizCard />
