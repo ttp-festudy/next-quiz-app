dc666b63b81e8654e5f04ce532c25a4be5a6fa51
rlawlsdn263 <rlawlsdn263@naver.com>
2024-04-03 00:13:49 +0900

    [🔨수정] `QuizModal` -> `SolutionCard`로 변경

--- src/app/solution/[id]/_components/SolutionCard.tsx
+++ src/app/solution/[id]/_components/SolutionCard.tsx
@@ -1,13 +1,13 @@
 import { cs } from "@/data/_index";
 
-export default function QuizModal() {
+export default function SolutionCard({ id }: { id: number }) {
   return (
     <div className="w-[800px] h-[400px] bg-[#6d6d6d] p-[20px] rounded-[20px] flex flex-col gap-[20px]">
       <div className="bg-[#4f4f4f] p-[20px] rounded-[10px]">
-        <span className="font-[800]">{cs[0].title}</span>
+        <span className="font-[800]">{cs[id].title}</span>
       </div>
       <div className="bg-[#4f4f4f] p-[20px] rounded-[10px] flex-1">
-        <p>{cs[0].content}</p>
+        <p>{cs[id].content}</p>
       </div>
     </div>
   );
---
+++ src/app/solution/[id]/_components/_index.ts
@@ -0,0 +1,1 @@
+export { default as SolutionCard } from "./SolutionCard";
