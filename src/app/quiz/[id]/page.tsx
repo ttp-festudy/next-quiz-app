0d3112e6775c83c4b6cc0fb797c9f7b96ff0c9d8
rlawlsdn263 <rlawlsdn263@naver.com>
2024-04-02 23:29:55 +0900

    [🗼마크업] `QuizPage` 컴포넌트 생성

---
+++ src/app/quiz/[id]/page.tsx
@@ -0,0 +1,9 @@
+import { QuizCard } from "./_components/_index";
+
+export default function QuizPage() {
+  return (
+    <main className="flex justify-center items-center h-screen">
+      <QuizCard />
+    </main>
+  );
+}
